31135f81054ec7e48bae2e5bfb26ba66cd1219ff
Rich Hildred <rhildred@gmail.com>
2023-03-31 17:06:20 -0400

    Updates

--- assets/pages/contact.page.7b282786.js
+++
@@ -1,3 +0,0 @@
-import{j as n,a as o,F as s}from"../chunks/0e184ced.js";import{M as c,a as r,b as i}from"../chunks/dd70bd71.js";const h={title:"Contact us",description:"Contact us about the next great thing"};function a(t){const e=Object.assign({h1:"h1",h2:"h2"},t.components);return o(s,{children:[n(c,{}),`
-`,o(r,{children:[n(e.h1,{children:h.title}),n(e.h2,{children:"We would love to hear from you."})]}),`
-`,n(i,{})]})}function l(t={}){const{wrapper:e}=t.components||{};return e?n(e,Object.assign({},t,{children:n(a,t)})):a(t)}export{l as default,h as documentProps};
