9182b54e6053600947675074d9d35f240e89d2ad
Rich Hildred <rhildred@gmail.com>
2023-04-03 14:55:01 -0400

    Updates

---
+++ assets/components/DaysLeft.1ab6afd0.js
@@ -0,0 +1,1 @@
+import{j as t}from"../chunks/0e184ced.js";function u(){let e=(new Date("2023-04-17T18:53:09.858Z")-new Date)/(1e3*3600*24);return t("span",{children:Math.round(e)})}export{u as default};
